817c741ed84302603677cec18a5cf155200b856d
su <2296887348@qq.com>
2020-04-30 16:57:44 +0800

    oauth模块

---
+++ spring-boot-oauth/src/main/java/com/soft1851/oauth/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.soft1851.oauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author su
+ * @className TestController
+ * @Description TODO
+ * @Date 2020/4/29
+ * @Version 1.0
+ **/
+@RestController
+public class TestController {
+
+    @RequestMapping("/index")
+    public String simpleIndex(){
+        return "index";
+    }
+}
